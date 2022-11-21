33166ff3e3f7073deab64e19b74fbee55c2279bb
Nathan Hefner <nxhefner@gmail.com>
2022-11-21 14:21:05 -0800

    testing print is close but still not there

--- code_fellows/linked_list.py
+++ code_fellows/linked_list.py
@@ -4,12 +4,21 @@
 class LinkedList:
     def __init__(self, values=None):
         self.head = None
+        self._length = 0
         if values:
             for value in values:
                 self.head = Node(value, next=self.head)
+                self._length += 1
+
+    def __len__(self):
+        return self.size()
+
+    def __print__(self):
+        return self.display()
 
     def push(self, value):
         self.head = Node(value, next=self.head)
+        self._length += 1
 
     def size(self):
         count = 0
@@ -22,10 +31,8 @@ class LinkedList:
     def search(self, value):
         if self.head == None:
             return None
-        count = 1
         current_node = self.head
         while value != current_node.value:
-            count += 1
             current_node = current_node.next
         if value == current_node.value:
             return current_node
@@ -36,19 +43,19 @@ class LinkedList:
         try:
             head_pop = self.head
             self.head = self.head.next
+            self._length += 1
             return head_pop
         except AttributeError:
             raise ValueError("Empty list try again")
 
     def display(self):
         list_output = []
-        current_value = ""
         current_node = self.head
         while current_node:
             list_output.append(current_node.value)
             current_node = current_node.next
         list_output = list_output[::-1]
-        return tuple(list_output)
+        return f"{tuple(list_output)}"
 
 
 class Node:
@@ -58,5 +65,5 @@ class Node:
 
 
 if __name__ == "__main__":
-    ll = LinkedList([3, "three"])
+    ll = LinkedList([3, "three", "nine"])
     ll_size = ll.size()
