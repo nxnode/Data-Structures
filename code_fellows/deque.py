6849d4aeeea1f333b9a53ede9fe396e9f6c45896
Nathan Hefner <nxhefner@gmail.com>
2023-01-18 16:00:01 -0800

    removed comments

--- code_fellows/deque.py
+++ code_fellows/deque.py
@@ -1,15 +1,5 @@
 # https://codefellows.github.io/sea-python-401d6/assignments/deque.html
 
-# Deque features:
-
-# append(val): adds value to the end of the deque
-# appendleft(val): adds a value to the front of the deque
-# pop(): removes a value from the end of the deque and returns it (raises an exception if the deque is empty)
-# popleft(): removes a value from the front of the deque and returns it (raises an exception if the deque is empty)
-# peek(): returns the next value that would be returned by pop but leaves the value in the deque (returns None if the deque is empty)
-# peekleft(): returns the next value that would be returned by popleft but leaves the value in the deque (returns None if the deque is empty)
-# size(): returns the count of items in the queue (returns 0 if the queue is empty)
-
 
 class Deque:
     def __init__(self, value=None):
