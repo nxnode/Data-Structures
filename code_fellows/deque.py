40b58827ef7fb5a7c018d89b3e3edbe142aecf55
Nathan Hefner <nxhefner@gmail.com>
2023-01-23 18:35:44 -0800

    pausing

--- code_fellows/deque.py
+++ code_fellows/deque.py
@@ -6,96 +6,69 @@ class Deque:
         self.head = None
         self.tail = None
         self._length = 0
-        self.is_empty = True
 
     def __len__(self):
-        self.is_empty = self._length == 0
         return self._length
 
     def size(self):
         return self._length
 
-    def append(self, value):
-        node = Node(value)
-        if not self.is_empty:
-            node.previous = self.tail
-            if self.tail:
-                self.tail.next = node
-            else:
-                self.head = node
-            self.tail = node
-            self._length += 1
-            self.is_empty = self._length == 0
-        else:
-            self.head = node
-            self.tail = node
-            self._length += 1
-            self.is_empty = self._length == 0
-
+    # HEAD
     def appendleft(self, value):
         node = Node(value)
-        if not self.is_empty:
-            node.next = self.head
-            if self.head:
-                self.head.previous = node
-            else:
-                self.tail = node
-            self.head = node
-            self._length += 1
-            self.is_empty = self._length == 0
+        node.next = self.head
+        if self.head:
+            self.head.previous = node
         else:
-            self.head = node
             self.tail = node
-            self._length += 1
-            self.is_empty = self._length == 0
+        self.head = node
+        self._length += 1
 
+    # HEAD
     def popleft(self):
-        if not self.is_empty:
-            popped_head = self.head
-            if self.head:
-                old_head_next = self.head.next
-                self.head = old_head_next
-                self._length -= 1
-                self.is_empty = self._length == 0
-                return popped_head.value
-            else:
-                raise ValueError("No head to pop")
+        popped_head = self.head
+        if self.head:
+            self.head = self.head.next
+            self._length -= 1
+            return popped_head.value
         else:
             raise ValueError("No head to pop")
 
+    # HEAD
     def peekleft(self):
-        if not self.is_empty:
-            popped_head = self.head
-            if self._length == 0:
-                return None
-            if self.head:
-                return popped_head.value
-            else:
-                return None
+        popped_head = self.head
+        if self.head:
+            return popped_head.value
         else:
             return None
 
+    # TAIL
+    def append(self, value):
+        node = Node(value)
+        node.previous = self.tail
+        if self.tail:
+            self.tail.next = node
+        else:
+            self.head = node
+        self.tail = node
+        self._length += 1
+
+    # TAIL
     def pop(self):
-        if not self.is_empty:
-            popped_tail = self.tail
-            if self.tail:
-                old_tail_prev = self.tail.previous
-                self.tail = old_tail_prev
-                self._length -= 1
-                self.is_empty = self._length == 0
-                return popped_tail.value
-            else:
-                raise ValueError("No tail to pop")
+        popped_tail = self.tail
+        if self.tail:
+            old_tail_prev = self.tail.previous
+            self.tail = old_tail_prev
+            self._length -= 1
+            return popped_tail.value
         else:
             raise ValueError("No tail to pop")
 
+    # TAIL
     def peek(self):
-        if not self.is_empty:
-            popped_tail = self.tail
-            if self.tail:
-                return popped_tail.value
-            else:
-                return None
+        popped_tail = self.tail
+        if self.tail:
+            return popped_tail.value
         else:
             return None
 
@@ -105,3 +78,9 @@ class Node:
         self.value = value
         self.next = None
         self.previous = None
+
+
+if __name__ == "__main__":
+    deq = Deque()
+    deq.append("tail")
+    deq.appendleft("head")
--- tests/code_fellows/test_deque.py
+++ tests/code_fellows/test_deque.py
@@ -7,15 +7,15 @@ from code_fellows.deque import Deque
 
 def test_deque():
     test_deq = Deque()
-    test_deq.appendleft("3")
+    test_deq.appendleft("head")
     assert test_deq.size() == 1
-    assert test_deq.peekleft() == "3"
-    test_deq.append("2")
+    assert test_deq.peekleft() == "head"
+    test_deq.append("tail")
     assert test_deq.size() == 2
-    assert test_deq.peek() == "2"
-    assert test_deq.peekleft() == "3"
-    assert test_deq.popleft() == "3"
-    assert test_deq.pop() == "2"
+    assert test_deq.peek() == "tail"
+    assert test_deq.peekleft() == "head"
+    assert test_deq.popleft() == "head"
+    assert test_deq.pop() == "tail"
     assert test_deq.size() == 0
     assert test_deq.peekleft() == None
     assert test_deq.peekleft() == None
@@ -25,15 +25,12 @@ def test_deque():
     with pytest.raises(ValueError) as error:
         test_deq.popleft()
     assert str(error.value) == "No head to pop"
-    assert test_deq.is_empty == True
     test_deq.append("new")
-    assert test_deq.is_empty == False
     assert test_deq.head.value == "new"
     assert test_deq.tail.value == "new"
     assert test_deq.pop() == "new"
     assert test_deq.size() == 0
     test_deq.appendleft("newLeft")
-    assert test_deq.is_empty == False
     assert test_deq.head.value == "newLeft"
     assert test_deq.tail.value == "newLeft"
     assert test_deq.popleft() == "newLeft"
